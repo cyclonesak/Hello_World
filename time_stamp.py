5cda76116b58fe41af61398d8573a19e8ad56a2f
cyclonesak <cyclonesak@gmail.com>
2024-01-24 14:54:21 +1000

    Final code

--- time_stamp.py
+++ time_stamp.py
@@ -2,8 +2,7 @@ from datetime import datetime
 
 def sak_TimeStamp():
     '''Output example: Wednesday 24-January-2024 2:35pm'''
-    date_time = datetime.now()
-    timeStamp = f'{date_time.strftime("%A %d-%B-%Y %I:%M%P").replace(" 0", " ")}'
+    timeStamp = f'{datetime.now().strftime("%A %d-%B-%Y %I:%M%P").replace(" 0", " ")}'
     return timeStamp
 
 if __name__ == '__main__':
