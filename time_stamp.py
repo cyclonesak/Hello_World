55b4f3d7eab4320b5485df13866a447da1646a70
Scott K <122759762+cyclonesak@users.noreply.github.com>
2024-01-24 14:38:35 +1000

    added comment to timestamp function

--- time_stamp.py
+++ time_stamp.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 def sak_TimeStamp():
+    '''Output example: Wednesday 24-January-2024 2:35pm'''
     date_time = datetime.now()
     timeStamp = f'{date_time.strftime("%A %d-%B-%Y %I:%M%P").replace(" 0", " ")}'
     return timeStamp
